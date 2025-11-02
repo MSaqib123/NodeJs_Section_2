bfc4d23600ed0101b9ec0e1c8d60d5e41e8bd208
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-11-02 22:34:41 +0500

    11 - Creating a Simple Web Server

--- index.js
+++ index.js
@@ -1,34 +1,63 @@
 
 //===================================
-// 10 - Reading and Writing Files Asynchronously
+// 11 - Creating a Simple Web Server
 //===================================
 //#region 
 const fs = require('fs');
-
-//==== Read Text ====
-// async method follow  callback function 
-
-// success 2nd  ya chlaa ge
-// fs.readFile('./txt/start.txt','utf-8',/*Call back function*/(err,data1)=>{
-fs.readFile('./txt/star.txt','utf-8',/*Call back function*/(err,data1)=>{
-    //create erro by wrong file name
-    if(err) return console.log(err);
-
-    fs.readFile(`./txt/${data1}.txt`,'utf-8',/*Call back function*/(err,data2)=>{
-        console.log(data2)
-        fs.readFile(`./txt/append.txt`,'utf-8',/*Call back function*/(err,data3)=>{
-            console.log(data3)
-            fs.writeFile('./txt/final.txt',`${data2}\n${data3}`, 'utf-8',error=>{
-                console.log('file has been written');
-            })
-        });
-    });
-});
-// success 1st phlaa ya chlaa ge
-console.log("will read file!");
+const http = require('http');
 
 
-//==== Write text ====
+// Server
+const server = http.createServer((req,res)=> {
+    // console.log(req)
+    res.end("Hello from Server");
+})
+
+server.listen(8000,'127.0.0.1',()=>{
+    console.log("Listening to request on port 8000");
+})
+
+//server keep runing until we do not stop
+//becuase of event loop used on server and listen
+// after runing the server  node index.js    
+// user server on browser   127.0.0.1:8000
+
+
+//#endregion
+
+
+
+
+// //===================================
+// // 10 - Reading and Writing Files Asynchronously
+// //===================================
+//#region 
+// const fs = require('fs');
+
+// //==== Read Text ====
+// // async method follow  callback function 
+
+// // success 2nd  ya chlaa ge
+// // fs.readFile('./txt/start.txt','utf-8',/*Call back function*/(err,data1)=>{
+// fs.readFile('./txt/star.txt','utf-8',/*Call back function*/(err,data1)=>{
+//     //create erro by wrong file name
+//     if(err) return console.log(err);
+
+//     fs.readFile(`./txt/${data1}.txt`,'utf-8',/*Call back function*/(err,data2)=>{
+//         console.log(data2)
+//         fs.readFile(`./txt/append.txt`,'utf-8',/*Call back function*/(err,data3)=>{
+//             console.log(data3)
+//             fs.writeFile('./txt/final.txt',`${data2}\n${data3}`, 'utf-8',error=>{
+//                 console.log('file has been written');
+//             })
+//         });
+//     });
+// });
+// // success 1st phlaa ya chlaa ge
+// console.log("will read file!");
+
+
+// //==== Write text ====
 
 //#endregion
 
