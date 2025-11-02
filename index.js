b68d1b39117e010b3163fbb81d48f8dbb29dfff1
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-11-02 19:51:35 +0500

    7 - Using Modules 1 Core Modules

---
+++ index.js
@@ -0,0 +1,19 @@
+
+//===================================
+// 7 - Using Modules 1 Core Modules
+//===================================
+//#region 7 - Using Modules 1 Core Modules
+// //======== Node Modules ========
+
+// // file system   = read
+// // https://nodejs.org/docs/latest-v20.x/api/fs.html   document
+// const fs = require(fs);
+
+
+
+
+// //======== code ========
+// const hello = 'Pakistan Zindabad'
+// console.log(hello);
+
+//#endregion
