601cee8a284397404ece2d16b2c185a556ebb6a1
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-11-04 13:16:26 +0500

    17 - Using Modules 2 Our Own Modules

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 //===================================
-// 16 - Parsing Variables from URLs
+// 17 - Using Modules 2 Our Own Modules
 //===================================
 //#region 
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+
+const replaceTemplate = require('./modules/replaceTemplate');
 
 
 
@@ -18,19 +20,18 @@ const templateCard = fs.readFileSync('./templates/template-card.html','utf-8');
 const templateProduct = fs.readFileSync('./templates/template-product.html','utf-8');
 
 // Helper Methods
-const replaceTemplate = (temp,product) =>{
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
-    output = output.replace(/{%IMAGE%}/g, product.image)
-    output = output.replace(/{%FROM%}/g, product.from)
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
-    output = output.replace(/{%DESCRIPTION%}/g, product.description)
-    output = output.replace(/{%QUANTITY%}/g, product.quantity)
-    output = output.replace(/{%ID%}/g, product.id)
-    output = output.replace(/{%PRICE%}/g, product.price)
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-
-    return output;
-}
+// const replaceTemplate = (temp,product) =>{
+//     let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+//     output = output.replace(/{%IMAGE%}/g, product.image)
+//     output = output.replace(/{%FROM%}/g, product.from)
+//     output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+//     output = output.replace(/{%DESCRIPTION%}/g, product.description)
+//     output = output.replace(/{%QUANTITY%}/g, product.quantity)
+//     output = output.replace(/{%ID%}/g, product.id)
+//     output = output.replace(/{%PRICE%}/g, product.price)
+//     if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+//     return output;
+// }
 
 
 // Server
